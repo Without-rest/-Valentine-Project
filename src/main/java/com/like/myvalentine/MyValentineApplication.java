678c1d1a4d51e65a573283e624c0b4a4a02557ce
ino5 <chero77@naver.com>
2022-11-08 07:10:38 +0900

    스프링 프로젝트 생성

---
+++ src/main/java/com/like/myvalentine/MyValentineApplication.java
@@ -0,0 +1,13 @@
+package com.like.myvalentine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyValentineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyValentineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/like/myvalentine/MyValentineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.like.myvalentine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyValentineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
